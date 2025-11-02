21eff45ee22bb50f810311baab97a7dd360d27ab
Homura <egzozu.be1.bas@gmail.com>
2025-11-02 10:07:20 +0300

    I forgor

--- source/mobile/flixel/system/FlxShader.hx
+++ source/mobile/flixel/system/FlxShader.hx
@@ -51,11 +51,7 @@ class FlxShader extends OriginalFlxShader
 		@:privateAccess
 		var gl = __context.gl;
 
-		#if lime_opengles
-		var prefix = "#version 300 es\n";
-		#else
-		var prefix = "#version 330\n";
-		#end
+		var prefix = '#version ${Flags.DEFAULT_GLSL_VERSION}\n';
 
 		#if (js && html5)
 		prefix += (precisionHint == FULL ? "precision mediump float;\n" : "precision lowp float;\n");
@@ -69,7 +65,6 @@ class FlxShader extends OriginalFlxShader
 			+ "#endif\n\n";
 		#end
 
-		#if lime_opengles
 		prefix += 'out vec4 output_FragColor;\n';
 		var vertex = prefix
 			+ glVertexSource.replace("attribute", "in")
@@ -77,10 +72,6 @@ class FlxShader extends OriginalFlxShader
 				.replace("texture2D", "texture")
 				.replace("gl_FragColor", "output_FragColor");
 		var fragment = prefix + glFragmentSource.replace("varying", "in").replace("texture2D", "texture").replace("gl_FragColor", "output_FragColor");
-		#else
-		var vertex = prefix + glVertexSource;
-		var fragment = prefix + glFragmentSource;
-		#end
 
 		var id = vertex + fragment;
 
